6fadb9e1bbb5c633e3718a84c9ed97eb9e55f34b
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2022-05-10 23:24:53 +0900

    setup

---
+++ setup.spec.js
@@ -0,0 +1,113 @@
+const initializeCityDatabase = () => {
+  console.log('cityDatabaseInitialized')
+}
+
+beforeEach(() => {
+  initializeCityDatabase();
+});
+
+const clearCityDatabase = () => {
+  console.log('cityDatabaseCleared')
+}
+
+afterEach(() => {
+  clearCityDatabase();
+});
+
+const isCity = (city) => {
+  return ['Vienna', 'San Juan'].includes(city)
+}
+
+test('city database has Vienna', () => {
+  expect(isCity('Vienna')).toBeTruthy();
+});
+
+test('city database has San Juan', () => {
+  expect(isCity('San Juan')).toBeTruthy();
+});
+
+beforeAll(() => {
+  return initializeCityDatabase();
+});
+
+afterAll(() => {
+  return clearCityDatabase();
+});
+
+beforeEach(() => {
+  return initializeCityDatabase();
+});
+
+const initializeFoodDatabase = () => {
+  console.log('foodDatabaseInitialized')
+}
+
+const isValidCityFoodPair = (city, food) => {
+  return (city === 'Vienna' && food === 'Wiener Schnitzel') ||
+    (city === 'San Juan' && food === 'Mofongo')
+}
+
+describe('matching cities to foods', () => {
+  // Applies only to tests in this describe block
+  beforeEach(() => {
+    return initializeFoodDatabase();
+  });
+
+  test('Vienna <3 veal', () => {
+    expect(isValidCityFoodPair('Vienna', 'Wiener Schnitzel')).toBe(true);
+  });
+
+  test('San Juan <3 plantains', () => {
+    expect(isValidCityFoodPair('San Juan', 'Mofongo')).toBe(true);
+  });
+});
+
+beforeAll(() => console.log('1 - beforeAll'));
+afterAll(() => console.log('1 - afterAll'));
+beforeEach(() => console.log('1 - beforeEach'));
+afterEach(() => console.log('1 - afterEach'));
+test('', () => console.log('1 - test'));
+describe('Scoped / Nested block', () => {
+  beforeAll(() => console.log('2 - beforeAll'));
+  afterAll(() => console.log('2 - afterAll'));
+  beforeEach(() => console.log('2 - beforeEach'));
+  afterEach(() => console.log('2 - afterEach'));
+  test('', () => console.log('2 - test'));
+});
+
+describe('outer', () => {
+  console.log('describe outer-a');
+
+  describe('describe inner 1', () => {
+    console.log('describe inner 1');
+    test('test 1', () => {
+      console.log('test for describe inner 1');
+      expect(true).toEqual(true);
+    });
+  });
+
+  console.log('describe outer-b');
+
+  test('test 1', () => {
+    console.log('test for describe outer');
+    expect(true).toEqual(true);
+  });
+
+  describe('describe inner 2', () => {
+    console.log('describe inner 2');
+    test('test for describe inner 2', () => {
+      console.log('test for describe inner 2');
+      expect(false).toEqual(false);
+    });
+  });
+
+  console.log('describe outer-c');
+});
+
+test.only('this will be the only test that runs', () => {
+  expect(true).toBe(false);
+});
+
+test('this test will not run', () => {
+  expect('A').toBe('A');
+});
