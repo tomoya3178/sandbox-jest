f1bb05ea9b58084c5909c2ac3fef9e6f5cd526de
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2022-05-10 22:20:58 +0900

    callback

---
+++ callback.spec.js
@@ -0,0 +1,31 @@
+const fetchData = (callback) => {
+  return callback(null, 'peanut butter')
+}
+
+test('the data is peanut butter', () => {
+  function callback(error, data) {
+    if (error) {
+      throw error;
+    }
+    expect(data).toBe('peanut butter');
+  }
+
+  fetchData(callback);
+});
+
+test('the data is peanut butter', done => {
+  function callback(error, data) {
+    if (error) {
+      done(error);
+      return;
+    }
+    try {
+      expect(data).toBe('peanut butter');
+      done();
+    } catch (error) {
+      done(error);
+    }
+  }
+
+  fetchData(callback);
+});
